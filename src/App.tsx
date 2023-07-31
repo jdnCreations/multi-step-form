84ce85a20e9b17aa375606bd40d1b102dcec3c39
Jordan Needham <jdncreations@hotmail.com>
2023-07-31 12:10:55 +0800

    show thank you card on confirm, fix step indicator styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import bgDesktop from './assets/images/bg-sidebar-desktop.svg';
 import advanced from './assets/images/icon-advanced.svg';
 import arcade from './assets/images/icon-arcade.svg';
 import pro from './assets/images/icon-pro.svg';
-import checkmark from './assets/images/icon-checkmark.svg';
-import { useEffect, useState } from 'react';
+import thank from './assets/images/icon-thank-you.svg';
+import {  useState } from 'react';
+import { useForm, SubmitHandler, UseFormRegister } from 'react-hook-form';
 
 export function AddOn(props: { title: string, subtitle: string, cost: number, suffix:string, addons:AddOn[], setAddons:Function}) {
 
@@ -75,7 +76,10 @@ export function Plan(props: { title: string, cost: number, icon: string, billing
   )
 }
 
-export function Steps(props: { step:number, plan:string, planCost:number, billing:string, addons:AddOn[], setStep:Function, setPlan:Function, setPlanCost:Function, setBilling:Function, setAddons:Function, toggleBilling:Function }) {
+export function Steps(props: { confirmed:boolean, register:UseFormRegister<Inputs>, onSubmit:SubmitHandler<Inputs>, step:number, plan:string, planCost:number, billing:string, addons:AddOn[], setStep:Function, setPlan:Function, setPlanCost:Function, setBilling:Function, setAddons:Function, toggleBilling:Function }) {
+
+   const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   const monthly = props.billing === "Monthly";
   const suffix = props.billing === "Monthly" ? "mo" : "yr";
@@ -103,20 +107,21 @@ export function Steps(props: { step:number, plan:string, planCost:number, billin
         <h1 className="font-bold text-marine-blue text-2xl">Personal info</h1>
         <p className="font-regular text-base text-cool-gray">Please provide your name, email address, and phone number.</p>
 
-        <form action="" id="step-1" className="flex flex-col gap-3">
+        <form onSubmit={handleSubmit(onSubmit)} id="info" className="flex flex-col gap-3">
           <div className="flex flex-col">
             <label className="text-marine-blue text-sm" htmlFor="name">Name</label>
-            <input className='border rounded-md py-2 px-4 font-medium' type="text" name="name" placeholder='e.g. Stephen King'/>
+            <input {...register("name", { required: true})} aria-invalid={errors.name ? "true" : "false"} className={`${errors.name && "border-red-500"} border rounded-md py-2 px-4 font-medium outline-none`} type="text" name="name" placeholder='e.g. Stephen King'/>
+            {errors.name && <p>{errors.name?.message}</p>}
           </div>
 
           <div className="flex flex-col">
             <label className="text-marine-blue text-sm" htmlFor="email">Email</label>
-            <input className='border rounded-md py-2 px-4 font-medium' type="text" name="email" placeholder='e.g. stephenking@lorem.com'/>
+            <input {...register("email", {required: true})} className={`${errors.email && "border-red-500"} border rounded-md py-2 px-4 font-medium outline-none`} type="text" name="email" placeholder='e.g. stephenking@lorem.com'/>
           </div>
 
           <div className="flex flex-col">
             <label className="text-marine-blue text-sm" htmlFor="phone">Phone</label>
-            <input className='border rounded-md py-2 px-4 font-medium' type="text" name="phone" placeholder='e.g. +1 234 567 890'/>
+            <input {...register("phone", {required: true})} className={`${errors.phone && "border-red-500"} border rounded-md py-2 px-4 font-medium outline-none`} type="text" name="phone" placeholder='e.g. +1 234 567 890'/>
           </div>
         </form>
       </div>
@@ -158,9 +163,10 @@ export function Steps(props: { step:number, plan:string, planCost:number, billin
         </div>
       </div>
     )}
+    
 
     {/* STEP FOUR */}
-    {props.step === 4 && (      
+    {(props.step === 4 && !props.confirmed) && (      
       <div className="bg-white rounded-xl flex flex-col px-6 py-8 relative z-10 mt-4 gap-4 shadow-lg">
         <h1 className="font-bold text-marine-blue text-2xl">Finishing up</h1>
         <p className="font-regular text-base text-cool-gray">Double-check everything looks OK before confirming.</p>
@@ -189,9 +195,17 @@ export function Steps(props: { step:number, plan:string, planCost:number, billin
             <p className='text-purplish-blue font-bold'>+${getTotal()}/{suffix}</p>
           </div>
       </div>
-
     )}
-    
+
+    {/* CONFIRMED */}
+    {(props.confirmed && props.step === 4) && (
+      <div className="bg-white rounded-xl flex flex-col px-6 py-20 relative z-10 mt-4 gap-4 shadow-lg items-center">
+          <img src={thank} alt="" className='w-12' />
+          <h1 className='text-2xl font-bold text-marine-blue'>Thank you!</h1>
+          <p className='text-cool-gray text-center'>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.</p>
+        </div>
+      )
+    }
   </div>
   );
 }
@@ -201,18 +215,32 @@ interface AddOn {
   cost: number
 }
 
+type Inputs = {
+  name: string,
+  email: string,
+  phone: string
+}
+
 function App() {
   const [step, setStep] = useState(1);
   const [plan, setPlan] = useState('Arcade');
   const [planCost, setPlanCost] = useState<number>(9);
   const [billing, setBilling] = useState('Monthly');
   const [addons, setAddons] = useState<Array<AddOn>>([]);
+  const [confirmed, setConfirmed] = useState(false);
+
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
   function toggleBilling() {
     if (billing === "Monthly")
       setBilling("Yearly");
     else
       setBilling("Monthly");
+  }
+
+  function handleConfirm() {
+    setConfirmed(true);
   }
 
   return (
@@ -227,27 +255,27 @@ function App() {
 
           <div className='relative flex items-center justify-center gap-4 text-white'>
             <div className="flex gap-2 items-center">
-              <p className={`${step === 1 ? "bg-white text-marine-blue" : "bg-transparent"} rounded-full border border-white h-8 aspect-square flex justify-center items-center`}>1</p>
+              <p className={`${step === 1 ? "bg-light-blue text-marine-blue border-light-blue" : "bg-transparent"} font-medium rounded-full border border-white h-8 aspect-square flex justify-center items-center`}>1</p>
               <div className="uppercase hidden md:block">
                 <p>Step 1</p>
                 <p className="font-bold text-white">Your Info</p>
               </div>
             </div>
             <div className="flex gap-2 items-center">
-              <p className={`${step === 2 ? "bg-white text-marine-blue" : "bg-transparent"} rounded-full border border-white h-8 aspect-square flex justify-center items-center`}>2</p>
+              <p className={`${step === 2 ? "bg-light-blue border-light-blue text-marine-blue" : "bg-transparent"} font-medium rounded-full border border-white h-8 aspect-square flex justify-center items-center`}>2</p>
               <div className="uppercase hidden md:block">
                 <p>Step 2</p>
                 <p className="font-bold text-white">Select Plan</p>
               </div></div>
             <div className="flex gap-2 items-center">
-              <p className={`${step === 3 ? "bg-white text-marine-blue" : "bg-transparent"} rounded-full border border-white h-8 aspect-square flex justify-center items-center`}>3</p>
+              <p className={`${step === 3 ? "bg-light-blue border-light-blue text-marine-blue" : "bg-transparent"} font-medium rounded-full border border-white h-8 aspect-square flex justify-center items-center`}>3</p>
               <div className="uppercase hidden md:block">
                 <p>Step 3</p>
                 <p className="font-bold text-white">Add-ons</p>
               </div>
             </div>
             <div className="flex gap-2 items-center">
-              <p className={`${step === 4 ? "bg-white text-marine-blue" : "bg-transparent"} rounded-full border border-white h-8 aspect-square flex justify-center items-center`}>4</p>
+              <p className={`${step === 4 ? "bg-light-blue border-light-blue text-marine-blue" : "bg-transparent"} font-medium rounded-full border border-white h-8 aspect-square flex justify-center items-center`}>4</p>
               <div className="uppercase hidden md:block">
                 <p>Step 4</p>
                 <p className="font-bold text-white">Summary</p>
@@ -256,16 +284,19 @@ function App() {
           </div>
         </div>
 
-        <Steps step={step} plan={plan} planCost={planCost} billing={billing} addons={addons} setStep={setStep} setPlan={setPlan} setPlanCost={setPlanCost} setBilling={setBilling} setAddons={setAddons} toggleBilling={toggleBilling} />
+        <Steps confirmed={confirmed} register={register} onSubmit={onSubmit} step={step} plan={plan} planCost={planCost} billing={billing} addons={addons} setStep={setStep} setPlan={setPlan} setPlanCost={setPlanCost} setBilling={setBilling} setAddons={setAddons} toggleBilling={toggleBilling} />
       </div>
-      <div className="fixed bottom-0 left-0 w-full bg-white py-9 flex items-center z-50">
+      <div className={`${confirmed ? "hidden" : "flex"} fixed bottom-0 left-0 w-full bg-white py-9 flex items-center z-50`}>
         {step > 1 && (
           <button className='absolute left-[1rem] font-bold text-cool-gray' onClick={() => setStep(step-1)}>Go Back</button>
         )}
         {step === 4 ? (
-          <button className='absolute right-[1rem] font-bold bg-purplish-blue px-4 py-2 rounded-md text-white'>Confirm</button>
+          <button className='absolute right-[1rem] font-bold bg-purplish-blue px-4 py-2 rounded-md text-white' onClick={handleConfirm}>Confirm</button>
         ) : (
-          <button className="absolute right-[1rem] bg-marine-blue px-4 py-2 rounded-md text-white font-medium" onClick={() => setStep(step+1)}>Next Step</button>
+          <button form='info' type='submit' className="absolute right-[1rem] bg-marine-blue px-4 py-2 rounded-md text-white font-medium" 
+          onClick={
+            () => setStep(step+1)
+          }>Next Step</button>
         )}
       </div>
     </div>
@@ -273,6 +304,3 @@ function App() {
 }
 
 export default App;
-
-// ETA 8 hours
-// START AT 8PM -> 10PM
